ec3d233bced77057f4b349f8df43f038b0eb4808
Roger Marsh <roger.marsh@solentware.co.uk>
2021-10-02 19:58:01 +0000

    Installation instructions changed to 'python -m pip install --user ...'.
    Fix link typos.
    
    Remove version number list from 'Help | About' and add references to
    useful 'python -m pip ...' commands.
    
    In setup.py derive dependency_links from install_requires.
    
    Bump version number.

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ from setuptools import setup
 if __name__ == "__main__":
 
     long_description = open("README").read()
+    install_requires=["solentware-misc==1.3.2"]
 
     setup(
         name="emailstore",
-        version="1.4.3",
+        version="1.4.4",
         description="Select and store emails from mailbox",
         author="Roger Marsh",
         author_email="roger.marsh@solentware.co.uk",
@@ -42,8 +43,11 @@ if __name__ == "__main__":
             "Intended Audience :: Developers",
             "Development Status :: 3 - Alpha",
         ],
-        install_requires=["solentware-misc==1.3.1"],
+        install_requires=install_requires,
         dependency_links=[
-            "http://solentware.co.uk/files/solentware-misc-1.3.1.tar.gz",
+            "-".join(required.split("==")).join(
+                ("http://solentware.co.uk/files/", ".tar.gz")
+            )
+            for required in install_requires
         ],
     )
